6d07f7ab10a952da4edd3e948bfd19284b6c7c3c
JPisaBrony <jdpohl789@gmail.com>
2015-09-25 16:11:09 -0500

    + selection

--- TwilightsRandomAdventure/TwilightsRandomAdventure/Selection.cs
+++ TwilightsRandomAdventure/TwilightsRandomAdventure/Selection.cs
@@ -4,9 +4,32 @@ namespace TwilightsRandomAdventure
 {
 	public class Selection
 	{
+		Boolean quit = false;
+		int option;
+
 		public Selection ()
 		{
 			
+		}
+
+		public void startSelection() {
+			while (!quit) {
+				if (Int32.TryParse (Console.ReadLine (), out option)) {
+					switch (option) {
+					case 1:
+						Dungeon mainDungeon = new Dungeon ();
+						break;
+					case 2:
+						quit = true;
+						break;
+					default:
+						Console.WriteLine ("Invald Option");
+						break;
+					}
+				} else {
+					Console.WriteLine ("Boi, watcho trying to do");
+				}
+			}
 		}
 	}
 }
